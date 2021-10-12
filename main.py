4fe56c14c50e938ac0a53272826981af10af793e
Karel van der Linden <65907741+kcvanderlinden@users.noreply.github.com>
2021-10-12 12:39:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import paho.mqtt.client as mqtt #import the client1
+import paho.mqtt.client as mqtt #import the client
 import re
+# import appdaemon.plugins.hass.hassapi as hass
 
 
 ############
@@ -10,6 +11,7 @@ def on_message(client, userdata, message):
     if 'action_duration' in msg_: # listening to de button press duration send by IKEA button (but is not registered as standard in HASS)
         button_duration = float(msg_[msg_.index('action_duration')+1]) # because the index of the actual duration is one more than the item called 'action_duration'
         print(button_duration)
+        # hass.call_service("<action>", entity_id="<entity>") # it shoul in the end update a static number in Home Assistant as an input for an automation
 
 broker_address="<IP of your MQTT broker>" 
 client = mqtt.Client("Testing_Jupyter_labs") #create new instance
